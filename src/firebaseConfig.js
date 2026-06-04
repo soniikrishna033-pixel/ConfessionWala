a290d30ff7f8f21addc60875b7efb436914942ce
soniikrishna033 <soniikrishna033@gmail.com>
2026-06-05 01:23:15 +0700

    Revert to native Firebase auth domain

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -9,7 +9,7 @@ import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "confessionwala.in",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
